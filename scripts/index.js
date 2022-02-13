2635b080185037422f8dfaa5a1a298480ff55a4f
Slain Raccoon <evgen4@shepard20.ru>
2022-02-13 19:19:33 +0300

    develop: complite

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,35 @@
 //элементы
 let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__info');
+let nameInput = document.querySelector('.popup__text_type_name');
+let jodInput = document.querySelector('.popup__text_type_job');
+let title = document.querySelector('.profile__name');
+let subtitle = document.querySelector('.profile__subtitle');
 
-console.log(popup);
+const editButton = document.querySelector('.profile__edit-btn');
+const closeButton = document.querySelector('.popup__close-btn');
 
-//открытие закрытие попапа
+
+//функция открытия и закрытия попапа
 function popupON() {
-  popup.classList.add('.popup__open');
+  popup.classList.add('popup_opened');
   nameInput.value = title.textContent;
-  jobInput.value = resercher.textContent;
+  jobInput.value = subtitle.textContent;
 }
 
 function popupOff() {
-  popup.classList.remove('.popup__open');
+  popup.classList.remove('popup_opened');
+}
+
+//функция отправки данных по кнопке "сохранить"
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  title.textContent = nameInput.value;
+  subtitle.textContent = jobInput.value;
+  popupOff();
 }
 
 //слушатели
-
+editButton.addEventListener('click', popupON);
+closeButton.addEventListener('click', popupOff);
+formElement.addEventListener('submit', formSubmitHandler);
