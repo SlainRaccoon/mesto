9d8466a76e520424009421d02f8b4d1ff8f859fe
Slain Raccoon <evgen4@shepard20.ru>
2022-03-12 14:33:32 +0300

    dev: add validate and refactor code

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,7 @@
-/** elements buttons */
-const formEdit = document.querySelector('.popup__form_type_edit');
+/** elements forms */
+const formElement = document.querySelector('#form-edit');
+const formPlace = document.querySelector('#form-place');
+const formElementSubmitButton = formElement.querySelector('.popup__save-btn');
 
 /** edit profile */
 const editButton = document.querySelector('.profile__edit-btn');
@@ -9,13 +11,16 @@ const closeEditPopup = popupEdit.querySelector('.popup__close-btn');
 /** new place */
 const placeButton = document.querySelector('.profile__add-btn');
 const popupPlace = document.querySelector('.popup-place');
+const cardAddFormElement = document.querySelector('#form-place');
 const closePlacePopup = popupPlace.querySelector('.popup__close-btn');
+const cardAddFormSubmitButton = cardAddFormElement.querySelector('.popup__save-btn');
 
 /** inputs edit profile */
-const nameInput = document.querySelector('.popup__text_type_name');
-const jobInput = document.querySelector('.popup__text_type_job');
+const nameInput = document.querySelector('#name-input');
+const jobInput = document.querySelector('#job-input');
 const editTitle = document.querySelector('.profile__name');
 const editSubtitle = document.querySelector('.profile__subtitle');
+//const submitEditButton = document.querySelector('.popup__save-btn');
 
 /** popup zoom image */
 const imageOpenPopup = document.querySelector('.popup-view');
@@ -26,9 +31,9 @@ const imageCloseButton = imageOpenPopup.querySelector('.popup__close-btn');
 /** template */
 const templateCards = document.querySelector('#cards').content;
 const gallery = document.querySelector('.gallery');
-const cardAddFormElement = document.querySelector('.popup__form_type_place');
-const placeInput = cardAddFormElement.querySelector('.popup__text_type_place');
-const linkInput = cardAddFormElement.querySelector('.popup__text_type_link');
+
+const placeInput = cardAddFormElement.querySelector('#place-input');
+const linkInput = cardAddFormElement.querySelector('#link-input');
 
 
 /** function create new card */
@@ -46,7 +51,7 @@ function addCard(itemName, itemLink) {
   galleryCrad.querySelector('.gallery__delete-btn').addEventListener('click', deleteCard);
 
   return galleryCrad;
-}
+};
 
 /** render 6 cards from massiv */
 initialCards.forEach(item => {
@@ -60,58 +65,49 @@ function openImage(evt) {
   cardImage.alt = imageTarget.alt;
   cardTitle.textContent = imageTarget.alt;
   openPopup(imageOpenPopup);
-}
+};
 
 /** function like */
 function pressLike(evt) {
   evt.target.classList.toggle('gallery__like-btn_active');
-}
+};
 
 /** function delete */
 function deleteCard(evt) {
   evt.target.closest('.gallery__list').remove();
-}
+};
 
 /** function open and close popup */
 function openPopup(anyPopup) {
   anyPopup.classList.add('popup_opened');
-  document.addEventListener('keydown', closePoppEscBtn(anyPopup)); /** add listener Esc button */
-}
+  /** add listener Esc button */
+  document.addEventListener('keydown', closePoppEscBtn(anyPopup)); 
+};
+
 const closePopup = function (anyPopup) {
   anyPopup.classList.remove('popup_opened');
-  document.removeEventListener('keydown', closePoppEscBtn(anyPopup)); /** remove listener Esc button */
-}
-
-/** function close popup on overlay */
-function closePopupOverlay(anyPopup) {
-  return function(evt) {
-    if (evt.target === evt.currentTarget) {
-      closePopup(anyPopup);
-    }
-  }
-}
-/** function close popup on Esc */
-function closePoppEscBtn(anyPopup) {
-  return function(evt) {
-    if (evt.key === 'Escape') {
-      closePopup(anyPopup);
-    }
-  }
-}
+  /** remove listener Esc button */
+  document.removeEventListener('keydown', closePoppEscBtn(anyPopup)); 
+};
 
 /** function open edit profile */
 function openProfilePopup() {
   nameInput.value = editTitle.textContent;
+  hideInputError(formElement, nameInput, setObject.inputErrorClass, setObject.errorClass);
   jobInput.value = editSubtitle.textContent;
+  hideInputError(formElement, jobInput, setObject.inputErrorClass, setObject.errorClass);
+  enableSubmitButton(formElementSubmitButton, setObject.inactiveButtonClass);
   openPopup(popupEdit);
-}
+};
 
 /** function open add place */
 function openPlacePopup() {
-  placeInput.value = null;
-  linkInput.value = null;
+  formPlace.reset();
+  hideInputError(cardAddFormElement, placeInput, setObject.inputErrorClass, setObject.errorClass);
+  hideInputError(cardAddFormElement, linkInput, setObject.inputErrorClass, setObject.errorClass);
+  disableSubmitButton(cardAddFormSubmitButton, setObject.inactiveButtonClass);
   openPopup(popupPlace);
-}
+};
 
 /** function handler submit profile form */
 function handlerProfileFormSubmit(evt) {
@@ -119,7 +115,7 @@ function handlerProfileFormSubmit(evt) {
   editTitle.textContent = nameInput.value;
   editSubtitle.textContent = jobInput.value;
   closePopup(popupEdit);
-}
+};
 
 /** function handler submit add place */
 function handlerPlaceFormSubmit(evt) {
@@ -127,12 +123,30 @@ function handlerPlaceFormSubmit(evt) {
   const newCardCreate = addCard(placeInput.value, linkInput.value);
   gallery.prepend(newCardCreate);
   closePopup(popupPlace);
-}
+};
+
+/** function close popup on overlay */
+function closePopupOverlay(anyPopup) {
+  return function(evt) {
+    if (evt.target === evt.currentTarget) {
+      closePopup(anyPopup);
+    };
+  };
+};
+
+/** function close popup on Esc */
+function closePoppEscBtn(anyPopup) {
+  return function(evt) {
+    if (evt.key === 'Escape') {
+      closePopup(anyPopup);
+    };
+  };
+};
 
 /** listener */
 editButton.addEventListener('click', openProfilePopup);
 closeEditPopup.addEventListener('click', () => closePopup(popupEdit));
-formEdit.addEventListener('submit', handlerProfileFormSubmit);
+formElement.addEventListener('submit', handlerProfileFormSubmit);
 
 placeButton.addEventListener('click', openPlacePopup);
 closePlacePopup.addEventListener('click', () => closePopup(popupPlace));
